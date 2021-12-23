2200d7ee956d4fe7a6d6ed67ee3537d781f63b91
deka4core <borodaenglish@gmail.com>
2021-12-23 23:09:49 +0300

    Added some constants New Class MAP main loop moved to function Added button sound and menu background

--- constants.py
+++ constants.py
@@ -4,3 +4,5 @@
 
 SIZE = WIDTH, HEIGHT = 1900, 1000
 FPS = 60
+TILE_SIZE = 32
+MAPS_DIR = 'data/maps'
--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 import pygame
 import os
 import sys
-
+import pytmx
 import menu
 from constants import *
-
-"""
-    Инициализация переменных
-"""
-pygame.init()
-screen = pygame.display.set_mode(SIZE)
-clock = pygame.time.Clock()
 
 
 # Загрузка изображения
@@ -23,19 +16,49 @@ def load_image(name, colorkey=None):
     return image
 
 
-MenuScreen = menu.Menu('background_menu.png', screen, load_image, clock)  # сцена с меню
+class Map:
 
-"""
-    Основной цикл
-"""
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.USEREVENT:
-            pass
-    screen.fill('black')
-    pygame.display.flip()  # смена кадра
-    clock.tick(FPS)  # установление ограничения кадров
+    def __init__(self, filename, free_tiles):
+        self.map = pytmx.load_pygame(f'{MAPS_DIR}/{filename}')
+        self.height = self.map.height
+        self.width = self.map.width
+        self.tile_size = self.map.tilewidth
+        self.free_tiles = free_tiles
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                image = self.map.get_tile_image(x, y, layer=0)
+                screen.blit(image, (x * self.tile_size, y * self.tile_size))
+
+    def get_tile_id(self, position):
+        return self.map.tiledgidmap[self.map.get_tile_gid(*position, layer=0)]
+
+    def is_free(self, position):
+        return self.get_tile_id(position) in self.free_tiles
+
+
+def main():
+    """
+            Инициализация переменных
+    """
+    pygame.init()
+    screen = pygame.display.set_mode(SIZE)
+    clock = pygame.time.Clock()
+    menu.Menu('background_menu.png', screen, load_image, clock)
+
+    Map('map.tmx', [30, 46])
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.USEREVENT:
+                pass
+        screen.fill('black')
+        pygame.display.flip()  # смена кадра
+        clock.tick(FPS)  # установление ограничения кадров
+
+
+main()
